4720ef0eb48f5bcc1c99ca2b3a058aed48fdbb0e
Stanislav Kuznichenko <stanislav.k@amayasoft.uz>
2022-11-12 17:34:01 +0300

    Added y offset generating mechanic.

--- Assets/Runtime/Core/CubeViewFactory.cs
+++ Assets/Runtime/Core/CubeViewFactory.cs
@@ -8,6 +8,8 @@ namespace Runtime.Core
 {
     public class CubeViewFactory
     {
+        private const int YPositionDivisorPower = 3;
+        
         private readonly CubeModelGameplayConfig _gameplayConfig;
         private readonly CubeVisualConfig _visualConfig;
 
@@ -17,18 +19,23 @@ namespace Runtime.Core
             _visualConfig = visualConfig;
         }
 
-        public GameObject[] CreateViews()
+        public IEnumerable<GameObject> CreateViews()
         {
+            var cubeViewObject = new GameObject();
+            
             var cubeList = new List<GameObject>();
 
-            for (int x = 0; x < _gameplayConfig.CubeSizeVector.x; x++)
-            for (int y = 0; y < _gameplayConfig.CubeSizeVector.y; y++)
-            for (int z = 0; z < _gameplayConfig.CubeSizeVector.x; z++)
+            for (var x = 0; x < _gameplayConfig.CubeSizeVector.x; x++)
+            for (var y = 0; y < _gameplayConfig.CubeSizeVector.y; y++)
+            for (var z = 0; z < _gameplayConfig.CubeSizeVector.x; z++)
             {
-                var cube = Object.Instantiate(_visualConfig.CubePrefab);
+                var cube = Object.Instantiate(_visualConfig.CubePrefab, cubeViewObject.transform);
                 cube.transform.localPosition = new Vector3(-x, -y, z);
                 cubeList.Add(cube);
             }
+
+            cubeViewObject.transform.position = new Vector3(cubeViewObject.transform.position.x, 
+                (float)_gameplayConfig.CubeSizeVector.y / YPositionDivisorPower);
 
             return cubeList.ToArray();
         }
