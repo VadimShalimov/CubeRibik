80e5f82c068fd7eeb91f22bbd57bbd015319c41b
Nataliya <Kimnataliya3258@gmail.com>
2022-11-17 21:06:07 +0300

    UIPlaceHolderViwe addition.

--- Assets/Runtime/Root/InitializingLifetimeScope.cs
+++ Assets/Runtime/Root/InitializingLifetimeScope.cs
@@ -2,7 +2,7 @@ using Assets.Runtime.Configs;
 
 using Runtime.Controllers;
 using Runtime.Core;
-
+using Runtime.Views;
 using UnityEngine;
 
 using VContainer;
@@ -16,6 +16,8 @@ namespace Runtime.Root
 
         [SerializeField] private CubeVisualConfig _cubeVisualConfig;
 
+        [SerializeField] private UIPlaceholderView _uiPlaceholderView;
+
         protected override void Configure(IContainerBuilder builder)
         {
             base.Configure(builder);
@@ -25,6 +27,8 @@ namespace Runtime.Root
             builder.Register<CubeViewFactory>(Lifetime.Singleton).WithParameter(_cubeModelGameplayConfig)
                 .WithParameter(_cubeVisualConfig);
 
+            builder.RegisterComponent(_uiPlaceholderView);
+            
             builder.RegisterEntryPoint<InitializingController>();
         }
     }
---
+++ Assets/Runtime/Views/UIPlaceholderView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Runtime.Views
+{
+    public class UIPlaceholderView : MonoBehaviour
+    {
+        public event Action ShuffleButtenClicked;
+        public event Action AssembleButtenClicked;
+
+        [SerializeField] private Button _shuffleCubeButton;
+        [SerializeField] private Button _assembleCubeButton;
+
+        private void Start()
+        {
+            _shuffleCubeButton.onClick.AddListener(() => ShuffleButtenClicked?.Invoke());
+            _assembleCubeButton.onClick.AddListener(() => AssembleButtenClicked?.Invoke());
+            
+            
+        }
+    }
+}
