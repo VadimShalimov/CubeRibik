832d2943ebde4d8dfcafb0a02d832227bd2c4b1a
Stanislav Kuznichenko <stanislav.k@amayasoft.uz>
2022-12-17 19:10:06 +0300

    Added cube rotation methods.

--- Assets/Runtime/Utils/RotationHelper.cs
+++ Assets/Runtime/Utils/RotationHelper.cs
@@ -21,17 +21,64 @@ namespace Runtime.Utils
             RotateMainSide(targetSide, rotateCondition);
         }
 
+        //fixed attached indexes bug
         private void RotateMainSide(SideModel mainSide, bool rotateCondition)
         {
             var replaceableValues = new List<ReplaceableValue<int>>();
 
             var sideIndexes = mainSide.PanelModel.GetSidesIndexes();
 
-            //todo: add rotation methods
+            if (rotateCondition)
+            {
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 0, 2, true);
+                
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 1, 0);
+                
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 3, 1, true, 1);
+
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 2, 3, false, 1);
+            }
+            else
+            {
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 2, 0, true);
+                
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 3, 2);
+                
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 1, 3, true, 1);
+
+                WriteReplaceableData(ref replaceableValues, sideIndexes, 0, 1, false, 1);
+            }
+            
+            mainSide.PanelModel.ReplaceIndexes(replaceableValues);
 
             foreach (var connectedSideModel in mainSide.ConnectedSideModels)
             {
                 connectedSideModel.PanelModel.ReplaceIndexes(replaceableValues);
+            }
+        }
+
+        private void WriteReplaceableData(ref List<ReplaceableValue<int>> replaceableValues, int[][] sideIndexes,
+            int startIndexSide, int lastIndexSide, bool reversible = false, int offset = 0)
+        {
+            if (reversible)
+            {
+                for (var i = 0; i < sideIndexes[startIndexSide].Length - 1; i++)
+                {
+                    var mainValue = sideIndexes[startIndexSide][i + offset];
+                    var replaceValue = sideIndexes[lastIndexSide][sideIndexes[3].Length - 1 - i - offset];
+
+                    replaceableValues.Add(new ReplaceableValue<int>(mainValue, replaceValue));
+                }
+            }
+            else
+            {
+                for (var i = 0; i < sideIndexes[startIndexSide].Length - 1; i++)
+                {
+                    var mainValue = sideIndexes[startIndexSide][i + offset];
+                    var replaceValue = sideIndexes[lastIndexSide][i + offset];
+
+                    replaceableValues.Add(new ReplaceableValue<int>(mainValue, replaceValue));
+                }
             }
         }
     }
