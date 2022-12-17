d569a434e265364409712acf3bfc8bd3f568e3c8
Stanislav Kuznichenko <stanislav.k@amayasoft.uz>
2022-12-17 17:32:26 +0300

    Added cube model graph initialization.

--- Assets/Runtime/Models/CubeModel.cs
+++ Assets/Runtime/Models/CubeModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Linq;
+
 using Runtime.Enums;
 using UnityEngine;
 
@@ -5,27 +8,58 @@ namespace Runtime.Models
 {
     public class CubeModel
     {
-        public Vector2 CubeLenght => _cubeLenght;
+        public Vector2Int CubeLenght => _cubeLenght;
         
         private readonly SideModel[] _sidesData;
-        private Vector2 _cubeLenght;
+        private Vector2Int _cubeLenght;
         
-        public CubeModel(SideModel[] sidesData, Vector2 cubeLenght)
+        public CubeModel(SideModel[] sidesData, Vector2Int cubeLenght)
         {
             _sidesData = sidesData;
             _cubeLenght = cubeLenght;
 
-            var cubeMatrixLength = 0;
-
-            foreach (var sideData in _sidesData)
-            {
-                cubeMatrixLength += sideData.MatrixLength;
-            }
+            ConfigureSidesGraphs();
+        }
+        
+        public SideModel GetSideModel(Side side)
+        {
+            return _sidesData.First(x => x.Side.Equals(side));
         }
 
-        public void RotateModel(Side rotatingSide, bool directionCondition, int deep)
+        private void ConfigureSidesGraphs()
         {
-            Debug.Log($"target Side{rotatingSide} and direction condition{directionCondition} with {deep}");
+            foreach (var sideModel in _sidesData)
+            {
+                switch (sideModel.Side)
+                {
+                    case Side.UpSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.FrontSide),GetSideModel(Side.RightSide), 
+                            GetSideModel(Side.BackSide), GetSideModel(Side.LeftSide));
+                        break;
+                    case Side.DownSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.FrontSide),GetSideModel(Side.RightSide),
+                            GetSideModel(Side.BackSide), GetSideModel(Side.LeftSide));
+                        break;
+                    case Side.LeftSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.UpSide),GetSideModel(Side.FrontSide),
+                            GetSideModel(Side.DownSide), GetSideModel(Side.BackSide));
+                        break;
+                    case Side.RightSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.UpSide),GetSideModel(Side.FrontSide),
+                            GetSideModel(Side.DownSide), GetSideModel(Side.BackSide));
+                        break;
+                    case Side.FrontSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.UpSide),GetSideModel(Side.RightSide),
+                            GetSideModel(Side.DownSide), GetSideModel(Side.LeftSide));
+                        break;
+                    case Side.BackSide:
+                        sideModel.SetConnectedSideModels(GetSideModel(Side.UpSide),GetSideModel(Side.RightSide),
+                            GetSideModel(Side.DownSide), GetSideModel(Side.LeftSide));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
         }
     }
 }
